ee31a0b12cc70c3553fc312b23e10a6208358db3
Sean Darcy <darcys22@gmail.com>
2019-12-07 13:10:33 +1100

    more formatting

--- pdfgenerator.js
+++ pdfgenerator.js
@@ -4,15 +4,28 @@ const util = require('util')
 const Handlebars = require('handlebars');
 const puppeteer = require('puppeteer');
 
+function commaFormat(number) {
+	return number.replace(/\B(?=(\d{3})+(?!\d))/g, ",").replace(/\B(?=(\d{2})+(?!\d))/g, ".");
+}
 
 
 (async function() {
   try {
 
     const data = {
-      name: 'Sean',
       total: '100',
-      due_date: 'Tuesday'
+      income: [
+        {account: "Sales", amount: "1000000"},
+        {account: "Other Income", amount: "12000"}
+      ],
+      expense: [
+        {account: "Operations", amount: "100000"},
+        {account: "Sales and Marketing", amount: "200000"},
+        {account: "Research and Development", amount: "120000"},
+        {account: "Depreciation and Amortisation", amount: "150000"},
+        {account: "Stuff", amount: "90000"},
+        {account: "Useless Expense", amount: "10000"},
+      ],
     }
 
     const browser = await puppeteer.launch();
@@ -20,6 +33,10 @@ const puppeteer = require('puppeteer');
 
     const templatePath = path.resolve('financials.html')
     var contents = fs.readFileSync(templatePath, 'utf8');
+
+		Handlebars.registerHelper('commaFormat', function(number) {
+			return commaFormat(number);
+		});
 
     const template = Handlebars.compile(contents)
     await page.setContent(template(data))
