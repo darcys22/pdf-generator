435f52876316692e40757ab6ccf52f8957445d30
Sean Darcy <darcys22@gmail.com>
2019-12-16 10:36:18 +1100

    Profit and Loss works

--- pdfgenerator.js
+++ pdfgenerator.js
@@ -4,10 +4,14 @@ const util = require('util')
 const Handlebars = require('handlebars');
 const puppeteer = require('puppeteer');
 
-const data = require('./data.json')
+const data = require('./output.json')
 
 function commaFormat(number) {
 	return (number/100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function creditCommaFormat(number) {
+	return (number/-100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 
@@ -24,9 +28,32 @@ function commaFormat(number) {
 			return commaFormat(number);
 		});
 
+		Handlebars.registerHelper('creditCommaFormat', function(number) {
+			return creditCommaFormat(number);
+		});
+
+    Handlebars.registerHelper('if_eq', function(a, b, opts) {
+				if (a == b) {
+						return opts.fn(this);
+				} else {
+						return opts.inverse(this);
+				}
+    });
+
+		Handlebars.registerHelper("debug", function(optionalValue) {
+			console.log("Current Context");
+			console.log("====================");
+			console.log(this);
+
+			if (optionalValue) {
+				console.log("Value");
+				console.log("====================");
+				console.log(optionalValue);
+			}
+		});
+
     const template = Handlebars.compile(contents)
-    await page.setContent(template(data))
-    //await page.setContent(contents)
+    await page.setContent(template({data}))
 
     await page.emulateMedia('screen');
     await page.pdf({
